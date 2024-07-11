a824221831587e41c6cf8fe39a5191435396bae1
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-11 14:02:08 +0900

    frontend push que button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/que">Que</RouterLink>
       </nav>
     </div>
   </header>
---
+++ frontend/src/views/QueView.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      message: '',
+      customProperty: '',
+      responseMessage: '',
+    };
+  },
+  methods: {
+    async sendMessage() {
+      try {
+        this.responseMessage = await fetch(`${import.meta.env.VITE_API_URL}/message`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            message: this.message,
+            customProperty: this.customProperty
+          })
+        }).then(response => response.json());
+      } catch (error) {
+        this.responseMessage = 'Failed to send message';
+        console.error(error);
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <h1>Send Message to Azure Service Bus Queue</h1>
+    <input v-model="message" placeholder="Enter your message" />
+    <input v-model="customProperty" placeholder="Enter custom property" />
+    <button @click="sendMessage">Send Message</button>
+    <p>{{ responseMessage }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
